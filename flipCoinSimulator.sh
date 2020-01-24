aec4686ea95ce3fb60d31f8a7279dd3562e6bda1
admin <rajnish.kahar1996@gmail.com>
2020-01-24 11:35:17 +0530

    ADDED:Percentage Of SingletFlip

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,27 @@
-#! /bin/bash
+#! /bin/bash -x 
 
 echo "Welcome to FlipCoinSimulator";
-singletCoinFlip=$((RANDOM%2))
-if [[ $singletCoinFlip -eq 0 ]]
+read -p "Enter the number of Coin flips: " num
+heads=0;
+tails=0;
+declare -A singleCoinFace
+
+function coinflip()
+{
+for (( i=0; i<$num; i++ ))
+do
+if [[ $((RANDOM%2)) -eq 0 ]]
 then
-	echo "It's a head";
+	((heads++))
+	singleCoinFace[Heads]=$heads
 else
-	echo "It's a tail";
+	((tails++))
+	singleCoinFace[Tails]=$tails
 fi
+done
+percentageOfHeads=$((${singleCoinFace[Heads]}*100/$num))
+percentageOfTails=$((${singleCoinFace[Tails]}*100/$num))
+}
+coinflip
+echo "Percentage of Heads: $percentageOfHeads"
+echo "Percentage of Tails: $percentageOfTails"
